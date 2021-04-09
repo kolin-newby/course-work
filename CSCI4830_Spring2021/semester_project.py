44fdd5c99b331982fe749c3dc6f8d1093fe0d2c7
Kolin Newby <kolin.newby@gmail.com>
2021-04-09 16:05:57 -0600

    Update semester_project.py

--- CSCI4830_Spring2021/semester_project.py
+++ CSCI4830_Spring2021/semester_project.py
@@ -2,19 +2,16 @@ from microbit import *
 from neopixel import *
 
 pixels = 16
+OFF = [0, 0, 0]
+RED = [10, 0, 0]
+RED_B = [50, 0, 0]
+GREEN = [0, 10, 0]
+YELLOW = [7, 3, 0]
 
 ring = NeoPixel(pin2, pixels)
 
-#red = [128, 0, 0]
-#green = [0, 128, 0]
-#yellow = [89, 39, 0]
-
-red = [8, 0, 0]
-green = [0, 8, 0]
-yellow = [6, 2, 0]
-
 def range_finder(raw, mx):
-    reading = (raw/mx)*100
+    reading = (raw/mx) * 100
 
     if reading >= 0 and reading <= 12.5:
         return 0
@@ -36,10 +33,31 @@ def range_finder(raw, mx):
         return 7
 
 
+def read_pulse():
+    raw_pulse_data = pin0.read_analog()
+    mx = 1023
+    ranged_pulse_data = range_finder(raw_pulse_data, mx)
+    return ranged_pulse_data
+
+
 while True:
+    top = read_pulse()
     for i in range(0, 8):
-        ring[i] = yellow
+        if i <= top:
+            ring[i] = GREEN
+        else:
+            ring[i] = OFF
+
+
     for i in range(8, 16):
-        ring[i] = green
+        ring[i] = RED
 
     ring.show()
+    sleep(50)
+
+
+    for i in range(8, 16):
+        ring[i] = RED_B
+
+    ring.show()
+    sleep(50)
